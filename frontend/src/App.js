6300e5506fa1786e780fad92d823bbdaf9980b71
manishpchauh-cell <manishpchauh@gmail.com>
2026-07-23 21:35:41 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+// App.js
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import ProjectPage from './pages/ProjectPage';
+import AddUser from './pages/AddUser';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/project/:projectId" element={<ProjectPage />} />
+      <Route path="/add-user" element={<AddUser />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+    </Routes>
+  );
+}
+
+export default App;
